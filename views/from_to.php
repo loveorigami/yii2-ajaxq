3668e1673a2d53f52779c81ef421b411ea4be7c2
Loveorigami <loveorigami@mail.ru>
2015-07-02 12:43:53 +0400

    change default id

--- views/from_to.php
+++ views/from_to.php
@@ -23,11 +23,11 @@ $this->registerJs($script);
 <div class="row col-md-12">
     <div class="form-group col-md-6">
         <label for="from">From</label>
-        <input type="text" class="form-control" id="from" placeholder="Enter from id" value="0">
+        <input type="text" class="form-control" id="from" placeholder="Enter from id" value="1">
     </div>
     <div class="form-group col-md-6">
         <label for="to">To</label>
-        <input type="text" class="form-control" id="to" placeholder="Enter to id" value="5">
+        <input type="text" class="form-control" id="to" placeholder="Enter to id" value="2">
     </div>
 
     <button type="submit" id="button-get" class="btn btn-primary">Submit</button>
